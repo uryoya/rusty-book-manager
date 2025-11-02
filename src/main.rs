ec9c33a16a99d58197dfe8a25189f403ae83d548
URANO Ryoya <urano.works.mail@gmail.com>
2025-11-02 18:00:25 +0900

    feat: 3.2 axumでサーバーを起動する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+// HTTPハンドラーの定義
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    // ルーターの定義
+    let app = Router::new().route("/hello", get(hello_world));
+
+    // localhost:8080 でリクエストを待ち受けるリスナーを作成
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    // サーバーを起動
+    axum::serve(listener, app).await.unwrap();
 }
